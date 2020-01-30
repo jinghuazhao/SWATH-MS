f6369ffdd62d621a82bdc3f8e24d85e2dea00fce
jinghuazhao <jinghuazhao@hotmail.com>
2020-01-30 15:59:58 +0000

    st.sh

---
+++ st.sh
@@ -0,0 +1,11 @@
+# 30-1-2020 JHZ
+
+git add README.md
+git commit -m "README"
+git add swath-ms.ini swath-ms.R
+git commit -m "Primary Code"
+git add utils
+git commit -m "utilities"
+git add st.sh
+git commit -m "st.sh"
+git push
