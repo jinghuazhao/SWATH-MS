4932b4709305f5da422b860b2a6913d7b6d44f34
jinghuazhao <jinghuazhao@hotmail.com>
2020-02-28 12:06:38 +0000

    utilities

--- utils/qctool.sh
+++ utils/qctool.sh
@@ -1,7 +1,8 @@
-# 27-2-2020 JHZ
+# 28-2-2020 JHZ
 
 export interval=/rds/project/jmmh2/rds-jmmh2-post_qc_data/interval/imputed/uk10k_1000g_b37/imputed
 qctool -g $interval/impute_#_interval.bgen -s $interval/interval.samples -incl-samples affymetrix.id \
        -og swath-ms.bgen -os swath-ms.samples
 
 qctool -g swath-ms-#.bgen -s swath-ms.sample -ofiletype binary_ped -og swath-ms.bgen
+plink --bfile swath-ms.bgen --maf 0.01 --make-bed --out swath-ms.01
---
+++ utils/tensorqtl.sh
@@ -0,0 +1,14 @@
+# 28-2-2020 JHZ
+
+# recycle tensorQTL venv
+# ln -s $HOME/tensorqtl/venv
+# pip install tensorqtl
+
+module load python/3.6
+virtualenv --system-site-package venv
+source venv/bin/activate
+hostname
+jupyter notebook --ip=127.0.0.1 --no-browser --port 8087
+# ssh -4 -L 8087:127.0.0.1:8087 -fN login-e-10.hpc.cam.ac.uk
+firefox http://127.0.0.1:8087/?token=d991ea12ce42b216d3aacd3c573e95280b6cd30d4b4aeeed &
+
