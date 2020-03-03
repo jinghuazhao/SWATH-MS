f7f7fbf919b4cfa3722068008073b81776173044
jinghuazhao <jinghuazhao@hotmail.com>
2020-03-03 12:05:46 +0000

    utilities

--- utils/plink2.sh
+++ utils/plink2.sh
@@ -4,8 +4,6 @@ export TMPDIR=/rds/user/$USER/hpc-work/
 
 module load plink/2.00-alpha
 
-plink --bfile swath-ms.bgen --maf 0.01 --make-bed --out swath-ms.01
-awk '{$1=$2};1' swath-ms.01.fam > swath-ms.fam
 seq 609 | \
 parallel -C' ' '
   export col=$(cut -d" " -f {} swath-ms.uniprot); \
@@ -22,3 +20,21 @@ parallel -C' ' '
       rm work/${v}.${col}.glm.linear
   done
 '
+
+seq 609 | \
+parallel -C' ' '
+  export col=$(cut -d" " -f {} swath-ms.uniprot); \
+  for v in ${col} ${col}_inv;
+  do
+      echo ${v}
+      plink2 \
+             --bgen swath-ms.01.bgen --sample swath-ms.sample \
+             --glm hide-covar --input-missing-phenotype -9 --covar-variance-standardize \
+             --pheno swath-ms.pheno --pheno-name ${col} --covar swath-ms.covar \
+             --out work/${v}
+      grep -v NA work/${v}.${col}.glm.linear | \
+      gzip -f > bgen/${v}-plink2.gz
+      rm work/${v}.${col}.glm.linear
+  done
+'
+
--- utils/qctool.sh
+++ utils/qctool.sh
@@ -1,7 +1,14 @@
-# 28-2-2020 JHZ
+# 3-3-2020 JHZ
 
 export interval=/rds/project/jmmh2/rds-jmmh2-post_qc_data/interval/imputed/uk10k_1000g_b37/imputed
+# SWATH-MS samples
 qctool -g $interval/impute_#_interval.bgen -s $interval/interval.samples -incl-samples affymetrix.id \
        -og swath-ms.bgen -os swath-ms.samples
-
+# MAF cutoff 0.01
 qctool -g swath-ms-#.bgen -s swath-ms.sample -ofiletype binary_ped -og swath-ms.bgen
+plink --bfile swath-ms.bgen --maf 0.01 --make-bed --out swath-ms.01
+awk '{$1=$2};1' swath-ms.01.fam > swath-ms.fam
+cut -f2 swath-ms.01.bim > swath-ms.01.snpids
+qctool -g swath-ms-#.bgen -s SomaLogic.sample -og swath-ms.01.bgen -bgen-bits 8 -incl-snpids swath-ms.01.snpids
+bgenix -g swath-ms.01.bgen -index -clobber
+
