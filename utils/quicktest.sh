58137e97b7262604c65277ab1a35ac50e8d30038
jinghuazhao <jinghuazhao@hotmail.com>
2020-03-03 21:19:12 +0000

    utilities

--- utils/quicktest.sh
+++ utils/quicktest.sh
@@ -5,17 +5,14 @@ export TMPDIR=/rds/user/$USER/hpc-work/
 seq 609 | \
 parallel -C' ' '
   export col=$(cut -d' ' -f {} swath-ms.uniprot)
-  for chr in $(seq 1 22)
-  do
-      quicktest --geno swath-ms.01.bgen --pheno swath-ms.sample --snptest --bgen \
-                --npheno ${col} --ncovar sex --ncovar age --ncovar bmi \
-                --ncovar PC1 --ncovar PC2 --ncovar PC3 --ncovar PC4 --ncovar PC5  \
-                --ncovar PC6 --ncovar PC7 --ncovar PC8 --ncovar PC9 --ncovar PC10 \
-                --ncovar PC11 --ncovar PC12 --ncovar PC13 --ncovar PC14 --ncovar PC15  \
-                --ncovar PC16 --ncovar PC17 --ncovar PC18 --ncovar PC19 --ncovar PC20 \
-                --method-mean --method-score --compute-rSqHat --out work/${col}-qt
-      grep -v NA work/${col}-qt | \
-      gzip -f > quicktest/${col}-qt.gz
-      rm work/${col}-qt
-  done
+  quicktest --geno swath-ms.01.bgen --pheno swath-ms.sample --snptest --bgen \
+            --npheno ${col} --ncovar sex --ncovar age --ncovar bmi \
+            --ncovar PC1 --ncovar PC2 --ncovar PC3 --ncovar PC4 --ncovar PC5  \
+            --ncovar PC6 --ncovar PC7 --ncovar PC8 --ncovar PC9 --ncovar PC10 \
+            --ncovar PC11 --ncovar PC12 --ncovar PC13 --ncovar PC14 --ncovar PC15  \
+            --ncovar PC16 --ncovar PC17 --ncovar PC18 --ncovar PC19 --ncovar PC20 \
+            --method-mean --method-score --compute-rSqHat --out work/${col}-qt
+  grep -v NA work/${col}-qt | \
+  gzip -f > quicktest/${col}-qt.gz
+  rm work/${col}-qt
 '
