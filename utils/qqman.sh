e43e079cfa8cd0a94ffd4247859ff5aec42ca0e1
jinghuazhao <jinghuazhao@hotmail.com>
2020-03-04 16:56:46 +0000

    utilities

--- utils/qqman.sh
+++ utils/qqman.sh
@@ -1,25 +1,28 @@
 # 4-3-2020 JHZ
 
 cat qqman.list | \
-parallel -C' ' '
+parallel -j1 -C' ' '
   echo {1} {2}
   export uniprot={1}
   export protein={2}
-  gunzip -c bgen/${uniprot}-plink2.gz | \
-  cut -f1,2,12 --output-delimiter=" " > ${uniprot}.txt
+  for v in ${uniprot} ${uniprot}_invn
+  do
+    gunzip -c bgen/${v}-plink2.gz | \
+    cut -f1,2,12 --output-delimiter=" " > ${v}.txt
 
-  R --slave --vanilla --args \
-    input_data_path=${uniprot}.txt \
-    output_data_rootname=${uniprot}_qq \
-    plot_title="${uniprot[$i]} (${protein})" < turboqq.r
+    R --slave --vanilla --args \
+      input_data_path=${v}.txt \
+      output_data_rootname=${v}_qq \
+      plot_title="${v} (${protein})" < turboqq.r
 
-  R --slave --vanilla --args \
-    input_data_path=${uniprot}.txt \
-    output_data_rootname=${uniprot}_man \
-    custom_peak_annotation_file_path=annotate.txt \
-    reference_file_path=turboman_hg19_reference_data.rda \
-    pvalue_sign=8.210181e-12 \
-    plot_title="${uniprot} (${protein})" < turboman.r
+    R --slave --vanilla --args \
+      input_data_path=${v}.txt \
+      output_data_rootname=${v}_man \
+      custom_peak_annotation_file_path=annotate.txt \
+      reference_file_path=turboman_hg19_reference_data.rda \
+      pvalue_sign=8.210181e-12 \
+      plot_title="${v} (${protein})" < turboman.r
 
-  rm ${uniprot}.txt
+    rm ${v}.txt
+  done
 '
