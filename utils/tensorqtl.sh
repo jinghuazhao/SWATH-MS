a191c67e626d3fb025f90a1703ec1f15c105f257
jinghuazhao <jinghuazhao@hotmail.com>
2020-03-06 11:36:31 +0000

    utilities

--- utils/tensorqtl.sh
+++ utils/tensorqtl.sh
@@ -1,4 +1,4 @@
-# 4-3-2020 JHZ
+# 6-3-2020 JHZ
 
 # recycle tensorQTL venv
 # ln -s $HOME/tensorqtl/venv
@@ -12,4 +12,17 @@ hostname
 jupyter notebook --ip=127.0.0.1 --no-browser --port 8087
 # ssh -4 -L 8087:127.0.0.1:8087 -fN login-e-10.hpc.cam.ac.uk
 firefox http://127.0.0.1:8087/?token=d991ea12ce42b216d3aacd3c573e95280b6cd30d4b4aeeed &
+
+export plink_prefix_path=swath-ms.01
+export expression_bed=swath-ms.expression.bed.gz
+export covariates_file=swath-ms.covariates.txt
+export prefix=swath-ms.sample
+
+python3 -m tensorqtl ${plink_prefix_path} ${expression_bed} ${prefix} \
+    --covariates ${covariates_file} \
+    --mode cis
+
+python3 -m tensorqtl ${plink_prefix_path} ${expression_bed} ${prefix} \
+    --covariates ${covariates_file} \
+    --mode trans
 
