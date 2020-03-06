f3638250238fa31f36a154f4c2dd2e0212c10331
jinghuazhao <jinghuazhao@hotmail.com>
2020-03-06 23:02:58 +0000

    utilities

--- utils/tensorqtl.sh
+++ utils/tensorqtl.sh
@@ -13,14 +13,23 @@ jupyter notebook --ip=127.0.0.1 --no-browser --port 8087
 # ssh -4 -L 8087:127.0.0.1:8087 -fN login-e-10.hpc.cam.ac.uk
 firefox http://127.0.0.1:8087/?token=d991ea12ce42b216d3aacd3c573e95280b6cd30d4b4aeeed &
 
-export plink_prefix_path=swath-ms.01
-export expression_bed=swath-ms.expression.bed.gz
 export covariates_file=swath-ms.covariates.txt
 export prefix=swath-ms.sample
+
+ln -sf swath-ms.01.bed 1.bed
+awk -v OFS="\t" '{$1="chr" $1};1' > 1.bim
+ln -sf swath-ms.01.fam 1.fam
+gunzip -c swath-ms.expression.bed.gz | grep -v chrX | gzip -f > 1.expression.bed.gz
+
+export plink_prefix_path=1
+export expression_bed=1.expression.bed.gz
 
 python3 -m tensorqtl ${plink_prefix_path} ${expression_bed} ${prefix} \
     --covariates ${covariates_file} \
     --mode cis
+
+export plink_prefix_path=swath-ms.01
+export expression_bed=swath-ms.expression.bed.gz
 
 python3 -m tensorqtl ${plink_prefix_path} ${expression_bed} ${prefix} \
     --covariates ${covariates_file} \
