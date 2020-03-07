bf751cdb2ffce8725bbbb18ce8b6793912db5960
jinghuazhao <jinghuazhao@hotmail.com>
2020-03-07 16:48:27 +0000

    st.sh

--- st.sh
+++ st.sh
@@ -1,8 +1,8 @@
-# 27-2-2020 JHZ
+# 7-3-2020 JHZ
 
 git add README.md
 git commit -m "README"
-git add swath-ms.ini swath-ms.R swath-ms.ipynb
+git add swath-ms.ini swath-ms.R swath-ms.ipynb swath-ms.sh
 git commit -m "Primary Code"
 git add utils
 git commit -m "utilities"
