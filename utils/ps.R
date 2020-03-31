4390ac2ce81df5d4af50a2dda0dff7d600b21564
jinghuazhao <jinghuazhao@hotmail.com>
2020-03-31 21:42:35 +0100

    utilities

---
+++ utils/ps.R
@@ -0,0 +1,34 @@
+# 31-3-2020 JHZ
+
+options(width=500)
+require(phenoscanner)
+catalogue <- "pQTL"
+rsid <- scan("swath-ms-invn.snp",what="")
+batches <- split(rsid, ceiling(seq_along(rsid)/100))
+s <- t <- list()
+for(i in 1:length(batches))
+{
+  q <- phenoscanner(snpquery=batches[[i]], catalogue=catalogue, proxies = "EUR", pvalue = 1e-07, r2= 0.6, build=37)
+  s[[i]] <- with(q,snps)
+  t[[i]] <- with(q,results)
+}
+snps <- do.call(rbind,s)
+results <- do.call(rbind,t)
+r <- list(snps=snps,results=results)
+save(r,file=paste0("swath-ms-invn.",catalogue))
+results <- within(results,{
+   a1 <- ref_a1
+   a2 <- ref_a2
+   swap <- ref_a1 > ref_a2
+   a1[swap] <- ref_a2[swap]
+   a2[swap] <- ref_a1[swap]
+   ref_snpid <- paste0(ref_hg19_coordinates,"_",a1,"_",a2)
+})
+for(d in unique(with(results,dataset)))
+{
+  cat(d,"\n")
+  sink(paste(catalogue,d,sep="."))
+  s <- subset(results[c("ref_rsid","ref_snpid","rsid","r2","p","efo","trait","dataset","pmid")],dataset==d)
+  print(s)
+  sink()
+}
