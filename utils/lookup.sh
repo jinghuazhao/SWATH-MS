5732bfa01d43f079560cceeaebb844dc02bc4ebb
jinghuazhao <jinghuazhao@hotmail.com>
2020-04-01 16:13:30 +0100

    utilities

--- utils/lookup.sh
+++ utils/lookup.sh
@@ -2,33 +2,33 @@
 
 function check()
 {
-  grep -H $1 swath-ms.merge
+  grep -H -w $1 swath-ms.merge
   export prot=$(grep $1 swath-ms.merge | cut -f5 | sed 's/_invn//g')
-  grep -H ${prot} $INF/doc/hgTables.tsv
-  grep -H ${prot} ~/SomaLogic/doc/SOMALOGIC_Master_Table_160410_1129info.tsv
+  grep -H -w ${prot} $INF/doc/hgTables.tsv
+  grep -H -w ${prot} ~/SomaLogic/doc/SOMALOGIC_Master_Table_160410_1129info.tsv
 }
 
 function Sun()
 {
-  grep $1 -H pQTL.Sun-B_pQTL_EUR_2017
+  grep $1 -H -w pQTL.Sun-B_pQTL_EUR_2017
   check $1
 }
 
 function Folkersen()
 {
-  grep $1 -H pQTL.Folkersen-L_Proteins_EUR_2017
+  grep $1 -H -w pQTL.Folkersen-L_Proteins_EUR_2017
   check $1
 }
 
 function Suhre()
 {
-  grep $1 -H pQTL.Suhre-K_pQTL_EUR_2017
+  grep $1 -H -w pQTL.Suhre-K_pQTL_EUR_2017
   check $1
 }
 
 function pQTL()
 {
-  grep $1 -H pQTL.pQTL_2017
+  grep $1 -H -w pQTL.pQTL_2017
   check $1
 }
 
