efa8b5af7a2e21808f34d98e69d075273187d10e
jinghuazhao <jinghuazhao@hotmail.com>
2020-04-01 15:58:34 +0100

    utilities

---
+++ utils/lookup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+
+function check()
+{
+  grep -H $1 swath-ms.merge
+  export prot=$(grep $1 swath-ms.merge | cut -f5 | sed 's/_invn//g')
+  grep -H ${prot} $INF/doc/hgTables.tsv
+  grep -H ${prot} ~/SomaLogic/doc/SOMALOGIC_Master_Table_160410_1129info.tsv
+}
+
+function Sun()
+{
+  grep $1 -H pQTL.Sun-B_pQTL_EUR_2017
+  check $1
+}
+
+function Folkersen()
+{
+  grep $1 -H pQTL.Folkersen-L_Proteins_EUR_2017
+  check $1
+}
+
+function Suhre()
+{
+  grep $1 -H pQTL.Suhre-K_pQTL_EUR_2017
+  check $1
+}
+
+function pQTL()
+{
+  grep $1 -H pQTL.pQTL_2017
+  check $1
+}
+
+for rsid in $(sed '1d' pQTL.Sun-B_pQTL_EUR_2017 | awk '{print $2}' | uniq )
+do
+  echo --- ${rsid} ---
+  Sun ${rsid}
+  echo 
+done
