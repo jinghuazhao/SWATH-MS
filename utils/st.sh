0815539ba42b179c72ad0c2abdfcd5761c3f6768
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-08-12 16:29:13 +0100

    utilities

--- utils/st.sh
+++ utils/st.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+git remote set-url origin https://github.com/jinghuazhao/SWATH-MS.git
 git add README.md
 git commit -m "README"
 git add swath-ms.ini swath-ms.R swath-ms.ipynb swath-ms.sh
